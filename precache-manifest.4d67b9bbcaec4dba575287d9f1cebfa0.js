187b108131dcd608176b0936c575aee48054a589
sigaeasu <sigikemahduta@gmail.com>
2021-10-18 14:55:04 +0700

    Updates

---
+++ precache-manifest.4d67b9bbcaec4dba575287d9f1cebfa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e8694464e7b71f9f62ebb3068d105d",
+    "url": "/react-movies-list/index.html"
+  },
+  {
+    "revision": "cd786090eb5e9332c732",
+    "url": "/react-movies-list/static/css/main.b4651a6a.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/react-movies-list/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-movies-list/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd786090eb5e9332c732",
+    "url": "/react-movies-list/static/js/main.b9e938cb.chunk.js"
+  },
+  {
+    "revision": "81e5c1959ac2014f191d",
+    "url": "/react-movies-list/static/js/runtime-main.ee98efb3.js"
+  }
+]);
