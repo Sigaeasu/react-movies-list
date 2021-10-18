3a563d47edbe0393fd091b2e31ac06838ac431bd
sigaeasu <sigikemahduta@gmail.com>
2021-10-18 11:32:44 +0700

    change api url into firebase google

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-1cc8f-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
